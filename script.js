7e2b76f139f0ce9a392ad71721b103f316e73f00
Pedro Queiroz <douglas.333.goncalves@gmail.com>
2023-01-24 00:30:00 -0300

    add plano de fundo

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-const form = document.getElementById("form-habits") // pegando o formulario pelo ID//
-const nlwSetup = new NLWSetup(form) //iniciar a biblioteca //
+const form = document.getElementById("form-habits") 
+const nlwSetup = new NLWSetup(form) 
 const button = document.querySelector("header button.add")
+const btnFundo1 = document.getElementById("btnFundo1") 
+const btnFundo2 = document.getElementById("btnFundo2") 
+const body = document.getElementById("body") 
+const header = document.getElementById("header") 
+
+
+
 button.addEventListener("click",add)
 form.addEventListener("change",save)
 function add(){
-    const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
-    
-   const dayExists = nlwSetup.dayExists(today)
+const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
 
-    if (dayExists){
-        alert("Dia ja incluso ")
-        return
-    }
+const dayExists = nlwSetup.dayExists(today)
+
+if (dayExists){
+alert("Dia ja incluso ")
+return
+}
 alert ("Dia adicioado com sucesso☑️")
 
 nlwSetup.addDay(today)
 }
 function save(){
-    localStorage.setItem("salvarDados",JSON.stringify(nlwSetup.data))
+localStorage.setItem("salvarDados",JSON.stringify(nlwSetup.data))
+}
+
+function alterarFundo1(){ 
+body.style.backgroundColor = '#09090A';
+header.style.backgroundColor = '#09090A';
+return 
+}
+function alterarFundo2(){ 
+body.style.backgroundColor = 'gray';
+header.style.backgroundColor = 'gray';
+return 
 }
 
 const data =JSON.parse(localStorage.getItem('salvarDados')) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
 
+
+btnFundo1.addEventListener("click", alterarFundo1)
+btnFundo2.addEventListener("click",alterarFundo2)
 
 
